3e18898557e0c4ab2667d660f52887e1397e48c0
iknoor12 <imiknoor@gmail.com>
2025-08-12 17:01:01 +0530

    Apply loop for displaying every quote from API.

--- script.js
+++ script.js
@@ -29,3 +29,34 @@ async function fetchData(){
         console.error(error);
     }
 }
+
+// let i = 0;
+// let allQuotes = [];
+
+// btn.addEventListener('click', fetchData);
+
+// async function fetchData() {
+//     quoteDiv.textContent = 'Loading..';
+    
+//     try {
+//         if (allQuotes.length === 0) {
+//             const response = await fetch("https://dummyjson.com/quotes");
+//             const data = await response.json();
+//             allQuotes = data.quotes;
+//         }
+
+//         // Loop through but break after showing one quote
+//         for (let count = 0; count < 1; count++) {
+//             quoteDiv.innerText = allQuotes[i].quote;
+//         }
+
+//         i++;
+//         if (i >= allQuotes.length) {
+//             i = 0; // reset to start
+//         }
+
+//     } catch (error) {
+//         quoteDiv.textContent = 'Oops! Something went wrong.';
+//         console.error(error);
+//     }
+// }
